0a821c9cc55a1bf8287c43822093d31a15f27ddd
29hoangvu <nvu08873@gmail.com>
2025-05-26 11:51:33 +0700

    add and show item to cart

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,6 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\FavoriteController;
 use App\Http\Controllers\Auth\LoginController;
-<<<<<<< Updated upstream
-=======
 use App\Http\Controllers\Admin\DashBoardController;
 use App\Http\Controllers\StaffController;
 use App\Http\Controllers\UserController;
@@ -29,7 +27,6 @@ Route::middleware(['role:user'])->group(function () {
     Route::get('/user/dashboard', [UserController::class, 'dashboard'])->name('user.dashboard'); // Dòng này quan trọng!
     Route::post('/user/logout', [UserController::class, 'logout'])->name('user.logout');
 });
->>>>>>> Stashed changes
 
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
@@ -63,10 +60,6 @@ Route::get('/products/search', [ProductController::class, 'search'])->name('prod
 Route::get('/products/{id}', [ProductController::class, 'show'])->name('products.show');
 Route::get('/products/{category}', [ProductController::class, 'category'])->name('products.category');
 
-<<<<<<< Updated upstream
-=======
 Route::get('/admin/index', [DashBoardController::class, 'index'])->name('admindashboard.index');
 
-
->>>>>>> Stashed changes
 require __DIR__.'/auth.php';
